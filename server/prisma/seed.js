5d2f4a53dfdc70e38e27cc8d0def5f844a4e6d4d
MarioPadilla <lladipas@hotmail.com>
2019-02-02 20:18:27 +0100

    prisma setup

---
+++ server/prisma/seed.js
@@ -0,0 +1,16 @@
+const { prisma } = require('../src/generated/prisma-client')
+
+async function main() {
+  await prisma.createUser({
+    name: 'Pepito',
+    type: 'CLIENT'
+  })
+  await prisma.createPost({
+    type: 'NEWS',
+    title: "Candelaria 2019",
+    image: '',
+    description: 'El 2 de Febrero en el poligono a las 19:00 '
+  })
+}
+
+main().catch(e => console.error(e))
--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -5,7 +5,19 @@ var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
+    name: "Post",
+    embedded: false
+  },
+  {
+    name: "PostType",
+    embedded: false
+  },
+  {
     name: "User",
+    embedded: false
+  },
+  {
+    name: "UserType",
     embedded: false
   }
 ];
