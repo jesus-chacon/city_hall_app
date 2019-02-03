f83c12b6d28e05d24d0a4e7643d8d5ded2cd20df
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2019-02-03 20:30:02 +0100

    Added vars environment to server and nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,10 @@ const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 const { Query } = require("./resolvers");
 
+const { SERVER_PORT, SERVER_GRAPHQL, SERVER_PLAYGROUND } = process.env;
+
+console.log(process.env, SERVER_PORT);
+
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
@@ -9,7 +13,17 @@ const server = new GraphQLServer({
   },
   context: {
     db: prisma
-  }
+  },
 });
 
-server.start(() => console.log("Running on localhost:4000"));
+server.start({
+  port: SERVER_PORT,
+  playground: SERVER_PLAYGROUND,
+  endpoint: SERVER_GRAPHQL
+}).then(() => {
+  console.log(
+    `Running
+      server --> localhost:${SERVER_PORT}${SERVER_GRAPHQL}
+      playground --> localhost:${SERVER_PORT}${SERVER_PLAYGROUND}`
+  );
+});
