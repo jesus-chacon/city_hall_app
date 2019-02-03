a5f7f8ab21cf677504e7fa8064b67e844d532904
MarioPadilla <lladipas@hotmail.com>
2019-02-03 16:49:05 +0100

    ionic: add two ways to observe graphql

--- ionic/src/app/home/home.page.ts
+++ ionic/src/app/home/home.page.ts
@@ -1,38 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+const Feed = gql`
+  query {
+    feed {
+      id
+      title
+      description
+    }
+  }
+`;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   loading = true;
   error: any;
   data: any[];
+  data2: Observable<any>;
+
+  private querySubscription: Subscription;
 
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-    this.apollo
-      .watchQuery({
-        query: gql`
-          {
-            feed {
-              id
-              title
-              description
-            }
-          }
-        `,
-      })
-      .valueChanges.subscribe(result => {
-        this.data = result.data && result.data['feed'];
-        this.loading = result.loading;
-        console.log(result);
-        this.error = !!result.errors;
-      });
+    this.querySubscription = this.apollo.watchQuery<any>({ query: Feed })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.data = data.feed;
+        this.loading = loading;
+      },
+        err => {
+          this.error = true;
+          this.loading = false;
+        });
+
+    this.data2 = this.apollo.watchQuery<any>({ query: Feed })
+      .valueChanges.pipe(map(({ data }) => data.feed));
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
   }
 
 }
